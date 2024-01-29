a76abfd8ed75e6587b8c8d03a9b044fa2540d4fd
Vitor Soier <vitorsoier1@gmail.com>
2024-01-29 15:55:15 -0300

    mudando a forma do log

--- backend/database/api.py
+++ backend/database/api.py
@@ -1,9 +1,8 @@
+import logging
 import requests
 import pandas as pd
-import logging
-
 from datetime import datetime
-from contracts.schema import GenericSchema, ApiSchema
+from contracts.schema import GenericSchema
 from typing import List
 from io import BytesIO
 
@@ -14,82 +13,62 @@ class ApiCollector:
         self.__schema = schema
         self.__buffer = None
 
-        logging.basicConfig(filename="backend/logs.log", level=logging.INFO)
+        logging.basicConfig(
+            filename="backend/logs.log",
+            level=logging.INFO,
+            format="%(message)s %(asctime)s",
+            datefmt="%m/%d/%Y %I:%M:%S %p",
+        )
         self.logger = logging.getLogger(__class__.__name__)
-        return
 
     def run_pipeline(self, param):
-        response = self.getData(param)
-        response = self.extractorData(response)
-        response = self.transformDf(response)
+        response = self.get_data(param)
+        response = self.extractor_data(response)
+        response = self.transform_df(response)
         return response
 
-    def getData(self, param):
+    def get_data(self, param):
         try:
             if param > 1:
                 response = requests.get(
                     f"http://127.0.0.1:8000/compra_produto/{param}"
                 ).json()
             else:
-                response = requests.get(f"http://127.0.0.1:8000/compra_produto").json()
-            self.logger.info(
-                f"Obtencao de dados realizada com sucecsso "
-                + datetime.now().strftime("%Y-%h-%d-%H:%M:%S")
-            )
-        except:
-            self.logger.warning(
-                f"Obtencao de dados falhou "
-                + datetime.now().strftime("%Y-%h-%d-%H:%M:%S")
-            )
+                response = requests.get("http://127.0.0.1:8000/compra_produto").json()
+            self.logger.info(f"Obtencao de dados realizada com sucesso: ")
+        except requests.RequestException as e:
+            self.logger.warning(f"Obtencao de dados falhou: ")
+            response = []
         return response
 
-    def extractorData(self, response):
+    def extractor_data(self, response):
+        result = []
         try:
-            result: List[GenericSchema] = []
             for compra in response:
-                dict_aux = {}
-                for key, value in self.__schema.items():
-                    if type(compra.get(key)) == value:
-                        dict_aux[key] = compra[key]
-                    else:
-                        dict_aux[key] = None
+                dict_aux = {
+                    key: compra[key] if isinstance(compra.get(key), value) else None
+                    for key, value in self.__schema.items()
+                }
                 result.append(dict_aux)
-            self.logger.info(
-                f"Obtencao de dados realizada com sucecsso "
-                + datetime.now().strftime("%Y-%h-%d-%H:%M:%S")
-            )
-        except:
-            self.logger.warning(
-                f"Extracao de dados falhou "
-                + datetime.now().strftime("%Y-%h-%d-%H:%M:%S")
-            )
+            self.logger.info(f"Extracao de dados realizada com sucesso: ")
+        except Exception as e:
+            self.logger.warning(f"Extracao de dados falhou: ")
         return result
 
-    def transformDf(self, response):
+    def transform_df(self, response):
         try:
             result = pd.DataFrame(response)
-            self.logger.info(
-                f"DF criado com sucecsso "
-                + datetime.now().strftime("%Y-%h-%d-%H:%M:%S")
-            )
-        except:
-            self.logger.warning(
-                f"Criacao DF falhou " + datetime.now().strftime("%Y-%h-%d-%H:%M:%S")
-            )
+            self.logger.info(f"DF criado com sucesso: ")
+        except Exception as e:
+            self.logger.warning(f"Criacao DF falhou: ")
+            result = pd.DataFrame()
         return result
 
-    def transforToParquet(self, response):
+    def transform_to_parquet(self, response):
         self.__buffer = BytesIO()
         try:
             response.to_parquet(self.__buffer)
-            self.logger.info(
-                f"parquet criado com sucecsso "
-                + datetime.now().strftime("%Y-%h-%d-%H:%M:%S")
-            )
-        except:
-            print("Error: não foi possível transformar o DF em parquet")
-            self.logger.warning(
-                f"Criacao parquet falhou "
-                + datetime.now().strftime("%Y-%h-%d-%H:%M:%S")
-            )
+            self.logger.info(f"Parquet criado com sucesso: ")
+        except Exception as e:
+            self.logger.warning(f"Criacao parquet falhou: ")
             self.__buffer = None
