eb58cd101fc4895c96679c1fd47e04e58838be0c
Vitor Soier <vitorsoier1@gmail.com>
2024-02-04 12:43:47 -0300

    Adicionando schedule para executar job para subir dados no gcs

--- backend/start.py
+++ backend/start.py
@@ -1,10 +1,17 @@
+import schedule
+import time
+
 from database.api import ApiCollector
 from contracts.schema import ApiSchema
 from google.cloud import storage
 
 storage_client = storage.Client()
-api = ApiCollector(ApiSchema).runPipeline(
-    5, storage_client, "vitorsoier-teste01-datawarehouse"
-)
 
-print(api)
+def ApiToBucket (schema, repeat, client, buckets_name):
+    api = ApiCollector(schema).runPipeline(repeat, client, buckets_name)
+
+schedule.every(1).minute.do(ApiToBucket, ApiSchema, 50, storage_client, 'vitorsoier-teste01-datawarehouse')
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
